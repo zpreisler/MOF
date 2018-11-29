d28a6e256e833d6d13d975fe4c296808fdcd84e8
Zdenek Preisler <z.preisler@gmail.com>
2018-11-29 15:26:31 -0800

    snapshot

---
+++ MOF_snapshot/1/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Job Name
+#SBATCH --job-name=sbap1
+
+# Partition:
+#SBATCH --partition=etna
+
+# Partition:
+#SBATCH --account=etna
+
+# Processors:
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=96:0:0
+
+# Send email post completion
+
+date '+RUN STARTED AT %m/%d/%y AT %H:%M:%S'
+
+#module load openmpi/2.0.2-intel
+
+time mpirun /global/home/users/preisler/Projects/MOF/kmc_mof_par
+
+echo ""
